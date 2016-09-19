7261094f838847e1de54380555606676f9d41e55
齐英杰 <qiyingjie@gyyx.cn>
2016-09-19 11:16:48 +0800

    引入CSS，JS静态文件

--- src/main/java/com/document/report/controller/HomeController.java
+++ src/main/java/com/document/report/controller/HomeController.java
@@ -2,6 +2,7 @@ package com.document.report.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * Created by Administrator on 2016/9/18.
@@ -13,5 +14,12 @@ public class HomeController {
     @RequestMapping(value = {"", "index"})
     public String index() {
         return "index";
+    }
+
+    @RequestMapping(value = "/signin", method = RequestMethod.POST)
+    public String signIn(String userid, String pwd, String code) {
+        // 登录验证
+
+        return "";
     }
 }
